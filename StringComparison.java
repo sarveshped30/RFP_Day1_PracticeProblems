d28f9b87ae322eddffa90496ac92b767a5eaca7e
Sarvesh pednekar <sarveshpednekar30@gmail.com>
2022-04-04 00:44:50 +0530

    Added StringComparison.java

---
+++ StringComparison.java
@@ -0,0 +1,12 @@
+public class StringComparison {
+	public static void main(String[] args) {
+		String name1 = "Sarvesh";
+		String name2 = "Sarvesh";
+		String name3 = "sarvesh";
+		String name4 = "Chaitanya";
+		
+		System.out.println(name1.equals(name2));
+		System.out.println(name1.equals(name3));
+		System.out.println(name1.equals(name4));
+	}
+}
